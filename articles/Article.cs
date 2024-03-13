e3cfe0cbce0fcacd90df8e79920ba3f93afb3523
sbahle.khawula@platform45.com <sbahle.khawula@platform45.com>
2024-03-13 18:20:28 +0200

    Add project files.

---
+++ articles/Article.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace articles
+{
+	public class Article
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string NewsSite { get; set; }
+		public DateTime PublishedAt { get; set; }
+	
+	}
+}
---
+++ articles/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using articles;
+using Newtonsoft.Json;
+
+var articles = GetArticles("articles.txt");
+if (articles != null)
+{
+	var response = GetNumberOfPublishedArticles(articles);
+	foreach (var r in response)
+	{
+		Console.WriteLine($"{r.Key} {r.Value}");
+	}
+}
+else
+{
+	Console.WriteLine("File is Empty");
+}
+
+
+ IList<Article>? GetArticles(string filepath)
+{
+	string articles = File.ReadAllText(filepath);
+	if(string.IsNullOrEmpty(articles))
+	{ 
+		return null;
+	}
+
+	var listOfArticles = JsonConvert.DeserializeObject<List<Article>>(articles);
+	return listOfArticles;
+}
+
+
+Dictionary<string, int> GetNumberOfPublishedArticles(IList<Article> articles)
+{
+	//from the dictionary idea that popped up at the end of our chat I was able to optimise and remove all those for loops like I explained
+	var monthlyPublishedArticles = new Dictionary<string, int>();
+
+	foreach (var article in articles)
+	{
+		var monthName = article.PublishedAt.ToString("MMMM");
+		var name = $"{monthName} {article.PublishedAt.Year} {article.NewsSite}";
+		if (monthlyPublishedArticles.ContainsKey(name))
+		{
+			monthlyPublishedArticles[name]++;
+		}
+		else
+		{
+			monthlyPublishedArticles.Add(name, 1);
+		}
+	}
+
+	return monthlyPublishedArticles;
+}
